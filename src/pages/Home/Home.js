fb0e1a584fd60fb9d354d8342a40fcdbf539f308
natthdias <nathalia.dias@nubank.com.br>
2022-06-28 15:55:54 -0300

    create navbar and card

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../../components/NavBar";
+import Card from "../../components/Card"
+
+const Home = () => (
+    <>
+    <NavBar/>
+    <Card/>
+    </>
+)
+
+export default Home;
